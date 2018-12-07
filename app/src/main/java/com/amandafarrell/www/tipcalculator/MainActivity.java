57b9857111d35067df5eed62cc6a69a06e62d9e1
Amanda Farrell <bluepandadev@gmail.com>
2018-12-07 11:51:28 -0700

    Update add TextWatcher to Split EditText

--- app/src/main/java/com/amandafarrell/www/tipcalculator/MainActivity.java
+++ app/src/main/java/com/amandafarrell/www/tipcalculator/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,16 +25,25 @@ public class MainActivity extends AppCompatActivity {
     private EditText mTipPercentEditText;
     private Integer mTipPercent;
     private String mTipPercentString;
-    private Button mTipPercentButtonIncrease;
     private Button mTipPercentButtonDecrease;
+    private Button mTipPercentButtonIncrease;
 
     private EditText mTipTotalEditText;
     private Integer mTipTotal;
     private String mTipTotalString;
 
+    private EditText mSplitEditText;
+    private Integer mSplit;
+    private Button mSplitButtonDecrease;
+    private Button mSplitButtonIncrease;
+
     private TextView mTotalTextView;
     private Integer mTotal;
     private String mTotalString;
+
+    private TextView mPerPersonTextView;
+    private Integer mPerPerson;
+    private String mPerPersonString;
 
 
     @Override
@@ -43,15 +54,20 @@ public class MainActivity extends AppCompatActivity {
         //Find views
         mBillEditText = (EditText) findViewById(R.id.bill);
         mTipPercentEditText = (EditText) findViewById(R.id.tip_percent);
-        mTipPercentButtonIncrease = (Button) findViewById(R.id.button_increase_tip_percent);
         mTipPercentButtonDecrease = (Button) findViewById(R.id.button_decrease_tip_percent);
+        mTipPercentButtonIncrease = (Button) findViewById(R.id.button_increase_tip_percent);
         mTipTotalEditText = (EditText) findViewById(R.id.tip_total);
+        mSplitEditText = (EditText) findViewById(R.id.split);
+        mSplitButtonDecrease = (Button) findViewById(R.id.button_decrease_split);
+        mSplitButtonIncrease = (Button) findViewById(R.id.button_increase_split);
         mTotalTextView = (TextView) findViewById(R.id.total);
+        mPerPersonTextView = (TextView) findViewById(R.id.total_per_person);
 
         //Initialize variables
         mBill = 0;
         mTipPercent = 15;
         mTipTotal = 0;
+        mSplit = 1;
         mTotal = 0;
 
         //Set on click listeners
@@ -75,11 +91,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        mSplitButtonDecrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mSplit > 1) {
+                    mSplit -= 1;
+                    setSplitEditText();
+                }
+            }
+        });
+
+        mSplitButtonIncrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mSplit += 1;
+                setSplitEditText();
+            }
+        });
+
         //Set text watchers
         mBillEditText.addTextChangedListener(billTextWatcher);
+        mSplitEditText.addTextChangedListener(splitTextWatcher);
 
         //Select all text when selected for each EditText
         mBillEditText.setSelectAllOnFocus(true);
+        mTipPercentEditText.setSelectAllOnFocus(true);
+        mTipTotalEditText.setSelectAllOnFocus(true);
+        mSplitEditText.setSelectAllOnFocus(true);
 
         setBillEditText();
     }
@@ -109,16 +148,15 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     mBill = Integer.parseInt(billStr);
                 }
-            }
-            catch (Exception e){
+            } catch (Exception e) {
                 Toast.makeText(getBaseContext(), R.string.large_number_error_toast, Toast.LENGTH_LONG).show();
 
                 mBill = 0;
             }
 
             setBillEditText();
-            if (mBillString.substring(mBillString.length()- 1 )
-                    .equals(NumberFormat.getCurrencyInstance().getCurrency().getSymbol())){
+            if (mBillString.substring(mBillString.length() - 1)
+                    .equals(NumberFormat.getCurrencyInstance().getCurrency().getSymbol())) {
                 mBillEditText.setSelection(mBillEditText.getText().length() - 2);
             } else {
                 mBillEditText.setSelection(mBillEditText.getText().length());
@@ -127,63 +165,116 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void afterTextChanged(Editable editable) {
-            setTotalEditText();
             setTipTotalEditText();
+            setTotalTextView();
+            setPerPersonTextView();
+        }
+    };
+
+    private TextWatcher splitTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            try {
+
+                mSplit = Integer.parseInt(mSplitEditText.getText().toString());
+
+            } catch (Exception e) {
+                Toast.makeText(getBaseContext(), R.string.large_number_error_toast, Toast.LENGTH_LONG).show();
+
+                mSplit = 1;
+                setSplitEditText();
+                mSplitEditText.setSelection(mSplitEditText.getText().length());
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            setPerPersonTextView();
         }
     };
 
     /**
      * Set mBillEditText to current value of mBill
      */
-    private void setBillEditText () {
+    private void setBillEditText() {
         NumberFormat format = NumberFormat.getCurrencyInstance();
-        mBillString = format.format(mBill/100.0);
+        mBillString = format.format(mBill / 100.0);
         mBillEditText.setText(mBillString);
     }
 
     /**
      * Set mTipPercentageEditText to current value of mTipPercent
      */
-    private void setTipPercentageEditText () {
+    private void setTipPercentageEditText() {
         mTipPercentString = String.valueOf(mTipPercent) + "%";
         mTipPercentEditText.setText(mTipPercentString);
     }
 
     /**
-     * Set mTipTotalEditText to current value of mTipTotal
+     * Set mTipTotalEditText to calculated value of mTipTotal
      */
-    private void setTipTotalEditText () {
+    private void setTipTotalEditText() {
 
         calculateTipTotal();
 
         NumberFormat format = NumberFormat.getCurrencyInstance();
-        mTipTotalString = format.format(mTipTotal/100.0);
+        mTipTotalString = format.format(mTipTotal / 100.0);
         mTipTotalEditText.setText(mTipTotalString);
+    }
+
+    /**
+     * Set mSplitEditText to value of mSplit
+     */
+    private void setSplitEditText() {
+        mSplitEditText.setText(String.valueOf(mSplit));
+    }
+
+    /**
+     * Set mTotalTextView to calculated value of mTotal
+     */
+    private void setTotalTextView() {
+
+        calculateTotal();
+
+        NumberFormat format = NumberFormat.getCurrencyInstance();
+        mTotalString = format.format(mTotal / 100.0);
+        mTotalTextView.setText(mTotalString);
     }
 
     /**
      * Set mTotalEditText to calculated value of mTotal
      */
-    private void setTotalEditText () {
+    private void setPerPersonTextView() {
 
-        calculateTotal();
+        calculatePerPersonTotal();
 
         NumberFormat format = NumberFormat.getCurrencyInstance();
-        mTotalString = format.format(mTotal/100.0);
-        mTotalTextView.setText(mTotalString);
+        mPerPersonString = format.format(mPerPerson / 100.0);
+        mPerPersonTextView.setText(mPerPersonString);
     }
 
     /**
      * Calculate tip total
      */
-    private void calculateTipTotal () {
-        mTipTotal = (mBill * mTipPercent)/100;
+    private void calculateTipTotal() {
+        mTipTotal = (mBill * mTipPercent) / 100;
     }
 
     /**
      * Calculate total
      */
-    private void calculateTotal () {
+    private void calculateTotal() {
         mTotal = mBill + mTipTotal;
+    }
+
+    /**
+     * Calculate per person total
+     */
+    private void calculatePerPersonTotal() {
+        mPerPerson = mTotal / mSplit;
     }
 }
